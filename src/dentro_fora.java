607f9623281301b6634ebcc2acfd584a5225fb40
Isaac Moraes de Melo <isaacmoraesdemelo@gmail.com>
2024-01-10 12:04:34 -0400

    problema: "dentro fora"

--- src/dentro_fora.java
+++ src/dentro_fora.java
@@ -3,9 +3,26 @@ import java.util.Scanner;
 
 public class dentro_fora {
     public static void main(String[] args) {
+        int ns;
+        int d=0;
+        int f=0;
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println();
+        System.out.print("quantos numeros você vai digitar? ");
+        int qn = sc.nextInt();
+
+        for(int a = 0; a < qn; a+=1){
+            System.out.println("digite um número: ");
+            int n = sc.nextInt();
+
+            if(n>=10 && n<=20){
+                d += 1;
+            }else{
+                f += 1;
+            }
+        }
+        System.out.printf("%d dentro\n",d);
+        System.out.printf("%d fora\n",f);
     }
 }
